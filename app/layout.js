cb595100d90e018e6f94ecdc47288a1b1f467673
LSBeryl <dltjgus8098@naver.com>
2024-05-26 13:01:32 +0900

    푸터 추가

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Menu from "../components/menu";
 import "../styles/global.css";
 
@@ -18,6 +19,16 @@ export default function RootLayout({ children }) {
         </div>
         <div className="component">
           <div>{children}</div>
+        </div>
+        <div className="footer">
+          <div>&copy; 2024 잡케. All rights reserved.</div>
+          <div>
+            <Link href="https://chzzk.naver.com/cad491fc20e7092e40615955cae88e80">
+              치지직
+            </Link>
+            •<Link href="https://discord.com/invite/v2s2F8FHFp">디스코드</Link>•
+            <Link href="https://www.youtube.com/@jobke_game">유튜브</Link>
+          </div>
         </div>
       </body>
     </html>
